d541e2e08ea8796f016c7828ce500e7db2c47500
laurianerobin <116274085+laurianerobin@users.noreply.github.com>
2023-03-20 11:59:18 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ Chez Propertize, nous comprenons que la vente ou l'achat d'une propriété est u
 
 Alors, pourquoi attendre ? Utilisez dès maintenant notre outil de prédiction de prix pour estimer la valeur de votre propriété.
 
+
 _BringItHome - Les clés qui vous donnent le pouvoir._
 ''')
 
@@ -290,7 +291,7 @@ pred_rounded = np.round(pred, 0)
 formatted_pred = format_decimal(int(pred_rounded), format='#,##0', locale='fr')
 
 # Afficher la prédiction en gras et en bleu
-st.write("Le prix de votre maison est estimé à ", f'<span style="color: blue;"><b>{formatted_pred} $</b></span>', unsafe_allow_html=True)
+st.write("Le prix de votre maison est estimé à ", f'<span style="color: blue;"><b>{formatted_pred} $</b></span>', "." unsafe_allow_html=True)
 
 # Ajouter un bouton "En savoir plus"
 

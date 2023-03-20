ba6c253fb597a157c71ce3d694a1277e6870be84
laurianerobin <116274085+laurianerobin@users.noreply.github.com>
2023-03-20 22:23:26 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -79,17 +79,25 @@ def user_input():
         GrLivArea_float = float(GrLivArea)
 
 
+  #### MS_zoning_RL : densité de l'endroit résidentiel
+  labels_MS_zoning_RL = [0,1]
+  options_MS_zoning_RL = {
+      1 :'Forte densité résidentielle',
+      0 : 'Faible densité résidentielle'
+      }
+  MS_zoning_RL = st.sidebar.radio("Densité du quartier", labels_MS_zoning_RL, format_func=lambda x: options_MS_zoning_RL[x])
+
+
   #### GardenSize : taille de la surface du jardin
   # On utilise st.checkbox() pour afficher une case à cocher
   GardenSize_want_option = st.sidebar.checkbox("Un espace extérieur entourant la maison ?")
 
   # Si la case est cochée, on affiche un curseur st.slider()
   if GardenSize_want_option:
-    GardenSize=st.sidebar.text_input("Précisez dans ce cas la surface extérieure de ce jardin (en mètres carrés)", value = 100)
       # Demander à l'utilisateur de saisir la valeur de la surface
     min_value_GardenSize = 10
     max_value_GardenSize = 3000
-    GardenSize = st.sidebar.text_input("Surface extérieure (en mètres carrés)", value = 100)
+    GardenSize = st.sidebar.text_input("Précisez dans ce cas la surface extérieure (en mètres carrés)", value = 100)
     
     # Vérifier que la saisie est valide
     try:
@@ -110,14 +118,6 @@ def user_input():
 
   else:
       GardenSize = 0 
-
-  #### MS_zoning_RL : densité de l'endroit résidentiel
-  labels_MS_zoning_RL = [0,1]
-  options_MS_zoning_RL = {
-      1 :'Forte densité résidentielle',
-      0 : 'Faible densité résidentielle'
-      }
-  MS_zoning_RL = st.sidebar.radio("Densité du quartier", labels_MS_zoning_RL, format_func=lambda x: options_MS_zoning_RL[x])
   
   
   ############### INTERIEUR

4f690b50f0a9a0340403b4368738d3b2e1ec2d27
laurianerobin <116274085+laurianerobin@users.noreply.github.com>
2023-03-20 11:56:13 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -290,7 +290,7 @@ pred_rounded = np.round(pred, 0)
 formatted_pred = format_decimal(int(pred_rounded), format='#,##0', locale='fr')
 
 # Afficher la prédiction en gras et en bleu
-st.write(f'<span style="color: blue;"><b>{formatted_pred} $</b></span>', unsafe_allow_html=True)
+st.write("Le prix de votre maison est estimé à ", f'<span style="color: blue;"><b>{formatted_pred} $</b></span>', unsafe_allow_html=True)
 
 # Ajouter un bouton "En savoir plus"
 

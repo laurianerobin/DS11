e694ad417837e4067e8a14e658b765c06cc06d2d
laurianerobin <116274085+laurianerobin@users.noreply.github.com>
2023-03-26 15:50:37 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -49,11 +49,9 @@ st.write('''
 ### Un prix, une maison, l'outil.
 Bienvenue chez _BringItOnHome_, l'agence immobilière innovante qui vous aide à **estimer la valeur de votre propriété en toute simplicité**. Nous comprenons que la vente ou l'achat d'une propriété est une décision importante, c'est pourquoi nous sommes déterminés à vous fournir les informations les plus précises possibles pour vous aider à prendre une **décision éclairée**.''')
 
-# Créer un lien hypertexte qui ouvre ou ferme le menu déroulant
-st.markdown('<a href="#" onclick="st.empty();">Le menu déroulant à gauche</a>', unsafe_allow_html=True)
+st.write(f"<span style='color:#2B92A5;'><b>Utilisez le volant déroulant à gauche pour nous décrire votre maison</b></span>. Il vous suffit de renseigner les caractéristiques de votre propriété, telles que la taille, l'emplacement et les équipements, pour obtenir une estimation immédiate.", unsafe_allow_html=True)
 
-# Forcer l'ouverture du menu déroulant
-st.empty()
+st.write("Notre outil de prédiction de prix utilise les dernières technologies d'apprentissage automatique pour fournir des estimations précises et fiables, en se basant sur des données du **marché immobilier américain**.")
 
 ### L'utilisateur répond à des questions et entre les paramètres correspondant à son souait, selon des variables regroupées 
 ### selon un "thème", comme l'aspect général de la maison, ses extérieurs, ou sa modernité.
